1731311f8892f42efc33693a9da2f6cc4235fd60
BhoomitRajpaL <narendra3263@gmail.com>
2025-10-06 18:36:49 +0530

    task 1 and task2

---
+++ task 1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+addition = num1 + num2
+print("addition: ", addition)
+subtraction = num1 - num2
+print("subtraction: ", subtraction)
+multiplication = num1 * num2
+print("multiplication: ", multiplication)
+division = num1 / num2
+print("division: ", division)
+ 
---
+++ task 2.py
@@ -0,0 +1,4 @@
+name = str(input("Enter your first name: "))
+surname = str(input("Enter your last name: "))
+
+print("hello, " + name + " " + surname + "! welcome to python programm")
